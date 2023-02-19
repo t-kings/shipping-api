565be99e5076939e9476bb206cd76ffedbb8b219
tkings <nwachukwu.kingsley01@yahoo.com>
2023-02-18 16:45:23 -0800

    :hard_check_mark: added tests

--- src/validation/calculate-shipping.validation.ts
+++ src/validation/calculate-shipping.validation.ts
@@ -15,20 +15,32 @@ export const getCalculateShippingPayloadValidationErrors = (
         pickup: string().required('Please provide a pickup address'),
         destination: string().required('Please provide a destination address'),
       }),
-      weight: number().required('Please provide the weight of the shipment'),
-      quantity: number().required('Please provide the quantity of items'),
-      costOfItem: number().required('Please provide the cost of each item'),
-      isVulnerable: boolean().required(
-        'Please specify if the item is vulnerable or not'
-      ),
-      length: number().required('Please provide the length of the shipment'),
-      width: number().required('Please provide the width of the shipment'),
-      height: number().required('Please provide the height of the shipment'),
+      weight: number()
+        .required('Please provide the weight of the shipment')
+        .typeError('Weight must be a number'),
+      quantity: number()
+        .required('Please provide the quantity of items')
+        .typeError('Quantity must be a number'),
+      costOfItem: number()
+        .required('Please provide the cost of each item')
+        .typeError('Cost of item must be a number'),
+      isVulnerable: boolean()
+        .required('Please specify if the item is vulnerable or not')
+        .typeError('Is vulnerable must be a boolean'),
+      length: number()
+        .required('Please provide the length of the shipment')
+        .typeError('Length must be a number'),
+      width: number()
+        .required('Please provide the width of the shipment')
+        .typeError('Width must be a number'),
+      height: number()
+        .required('Please provide the height of the shipment')
+        .typeError('Height must be a number'),
       dateOfShipping: string().required('Please provide the date of shipping'),
     });
 
     // Validate the request body against the schema
-    schema.validateSync(params);
+    schema.validateSync(params, { abortEarly: false });
 
     return null;
   } catch (error) {
